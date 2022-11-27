697d4cf8ea5b3b84412e3bcf146eed22bb3a3909
김정윤 <kjyjh9930@gmail.com>
2022-11-27 18:57:42 +0900

    chore : GA tracking id 재설정

--- user-meta-config.js
+++ user-meta-config.js
@@ -6,9 +6,9 @@ const userMetadata = {
   copyright: `Copyright ⓒ 2021 je0ngyun/JeongYun`,
   siteUrl: `https://je0ngyun.netlify.app`, // Enter the URL to deploy or deploy. Used to create a Sitemap and create Robots.txt.
   commentRepo: `je0ngyun/Blog`, // Please enter the Github repository where comments will be saved.
-  googleVerification: `gVXI8__9H5fEKSWQJR8C7GRAdoYu_qkhdlKiDvpZa0w`, // Please enter the verification code from Google Search Console.
-  naverVerification: `aa0d93dcccc985477f1ce8f20369c12c25cfba7e`, // Please enter the verification code from Naver Serarch Console.
-  googleAnalyticsTrackingId: `G-K1TM1WTDX4`, // Please enter the TrackingId from GoogleAnalytics.
+  googleVerification: ``, // Please enter the verification code from Google Search Console.
+  naverVerification: ``, // Please enter the verification code from Naver Serarch Console.
+  googleAnalyticsTrackingId: `G-2D1X7LND00`, // Please enter the TrackingId from GoogleAnalytics.
 }
 
 const pageMetadata = {
