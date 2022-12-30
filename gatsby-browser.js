e5b27a5f25db9e3fb58f02212c73c73c64f0896f
김정윤 <kjyjh9930@gmail.com>
2022-12-30 23:07:27 +0900

    feat : 기존 Layout 에서 적용된 global style을 gatsby-browser.js에서 import 하도록 변경

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 /* import typeface */
-require('typeface-catamaran')
-require('typeface-roboto-mono')
+import 'typeface-catamaran'
+import 'typeface-roboto-mono'
+import './src/styles/global.scss'
+import './src/styles/dark-mode.scss'
+import './src/styles/light-mode.scss'
